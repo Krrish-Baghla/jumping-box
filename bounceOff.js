174949b35a94da39cf635cd52b2b70c5de51b624
Krrish-Baghla <81738340+Krrish-Baghla@users.noreply.github.com>
2021-04-17 11:04:17 +0530

    Add files via upload

---
+++ bounceOff.js
@@ -0,0 +1,42 @@
+function bounceOff(object1,object2)
+{
+    if(object1.x - object2.x <object1.width/2+object2.width/2  && object2.x - object1.x < object1.width/2+object2.width/2
+        )
+        {
+        ball.velocityX = object1.velocityX *(-1);   
+        }
+            if(object1.y -object2.y < object1.height/2 + object2.height/2 && object2.y - object1.y < object2.height/2+object1.height/2)
+            {
+            ball.velocityY = object1.velocityY *(-1);   
+                
+            }
+     
+}
+function isTouching(object11,object12)
+{
+    if(object2.x - object1.x < object2.width/2+object1.width/2 && object1.x - object2.x < object1.width/2 + object2.width/2
+        && object2.y - object1.y < object2.height/2+object1.height/2 && object1.y<object1.height/2 + object2.height/2)
+    {
+     return true;
+    } 
+    else
+    {
+        return false;
+    }
+    /*if(object2.x - object1.x < object2.width/2+object1.width/2 && object1.x - object2.x < object1.width/2 + object2.width/2
+        && object2.y - object1.y < object2.height/2+object1.height/2 && object1.y<object1.height/2 + object2.height/2)
+        {
+            return true;
+        }
+    else{
+        return false;
+    }
+    if(object2.x - object1.x < object2.width/2+object1.width/2 && object1.x - object2.x < object1.width/2 + object2.width/2
+        && object2.y - object1.y < object2.height/2+object1.height/2 && object1.y<object1.height/2 + object2.height/2)
+        {
+            return true;
+        }
+    else{
+        return false;
+    }*/
+}
